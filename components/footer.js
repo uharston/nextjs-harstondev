860b3c21ec2dcbe5d7a5e009150f7555ad2e74fa
uharston <uharston@gmail.com>
2020-12-10 16:02:07 -0600

    Style Footer

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,15 @@
 
+import styles from '../styles/utils.module.css'
 
 function Footer() {
     return(
         <footer>
-            <div className='footer text-center border-top border-success' id="footer">
+            <div className={styles.footer} id="footer">
                 <p >"Hard Work Beats Talent"</p>
                 <p>This site was developed 💯% by Uriah Harston</p>
                 <p><a href="https://www.linkedin.com/in/uriah-harston/" target="_blank">Linkedin</a> | <a href="https://github.com/uharston" target="_blank">Github</a> | <a href="https://twitter.com/UriahHarston" target="_blank">Twitter</a></p>
             </div>
-            {/* <div className='footer text-center border-top border-success' id="footer"style="margin-top: 40px; padding-bottom: 25px;">
-                <p style="padding-top: 25px;">"Hard Work Beats Talent"</p>
-                <p>This site was developed 💯% by Uriah Harston</p>
-                <p><a href="https://www.linkedin.com/in/uriah-harston/" target="_blank">Linkedin</a> | <a href="https://github.com/uharston" target="_blank">Github</a> | <a href="https://twitter.com/UriahHarston" target="_blank">Twitter</a></p>
-            </div> */}
+        
         </footer>
     )
 }
