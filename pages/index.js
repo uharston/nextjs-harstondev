a02b6c603df853188c12068f5b1036bcec401539
uharston <uharston@gmail.com>
2020-12-10 14:42:04 -0600

    Import About and Contacts

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {Container, Nav, Navbar, Row, Col, Form, Button, Card } from 'react-bootstrap'
 import Landing from '../components/landing.js'
-import About from '../components/about.js'
 import Navigation from '../components/navigation.js'
+import About from '../components/about.js'
 import Projects from '../components/projects.js'
+import Contact from '../components/contact.js'
 import Footer from '../components/footer.js'
 
 export default function Home() {
