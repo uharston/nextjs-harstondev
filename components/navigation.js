6dbc00afb577f63704eddde363a0023f2720f5c0
uharston <uharston@gmail.com>
2020-12-10 15:53:08 -0600

    Navbar converted to React-Bootstrap | Aldrich Font Imported

--- components/navigation.js
+++ components/navigation.js
@@ -1,29 +1,20 @@
-import {Container, Nav, Navbar, Row, Col, Form, Button } from 'react-bootstrap'
+import {Nav, Navbar} from 'react-bootstrap'
+import styles from '../styles/utils.module.css'
 
 function Navigation() {
     return (
-        <Navbar className="navbar-expand-lg  navbar-custom sticky-top" id="navbar-text">
-            <a className="navbar-brand" href="#"><span id="navbar-logo">UH</span></a>
-            <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul className="navbar-nav ml-auto">
-                  <li className="nav-item ">
-                     <Nav.Link href="#about-me-section">About Me<span className="sr-only">(current)</span></Nav.Link>
-                  </li>
-                  <li className="nav-item">
-                     <Nav.Link href="#project-section">Projects</Nav.Link>
-                  </li>
-                  <li className="nav-item">
-                     <Nav.Link href="#contact-section" >Contact</Nav.Link>
-                  </li>
-               </ul>
-            </div>
-         </Navbar>
-        
+      <Navbar className={styles.navigation} variant="dark" expand="sm">
+         <Navbar.Brand href="#home">UH</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+               <Nav.Link href="#about-me-section">About Me<span className="sr-only">(current)</span></Nav.Link>
+               <Nav.Link href="#project-section">Projects</Nav.Link>
+               <Nav.Link href="#contact-section" >Contact</Nav.Link>
+            </Nav>
+         </Navbar.Collapse>
+      </Navbar> 
     ) 
-
 }
 
 export default Navigation
