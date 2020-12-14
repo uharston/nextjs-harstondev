102ace2bc5cbbf55e4dbff7184d811646f987279
uharston <uharston@gmail.com>
2020-12-14 14:09:19 -0600

    Imported and Centered Icons

--- components/landing.js
+++ components/landing.js
@@ -10,11 +10,13 @@ function Landing() {
                         <div className="thin" id="thin">I am Uriah Harston</div>
                         A Full Stack Developer
                     </h1>
-                    <div className="welcome-icons">
-                        <a href="https://www.linkedin.com/in/uriah-harston/" target="_blank"><FaLinkedin size='5rem'/></a>
-                        <a href="https://github.com/uharston" target="_blank"><FaGithub  size='5rem'/></a>
-                        <a href="https://twitter.com/UriahHarston" target="_blank"><FaTwitter  size='5rem'/></a>
-                        <a href="https://medium.com/@uharston" target="_blank"><FaMedium  size='5rem'/></a>
+                    <div className="welcome-icons text-center">
+                        <div className='welcome-icons-wrapper'>
+                            <a href="https://www.linkedin.com/in/uriah-harston/" target="_blank"><FaLinkedin className='landingIcons' /></a>
+                            <a href="https://github.com/uharston" target="_blank"><FaGithub  className='landingIcons' /></a>
+                            <a href="https://twitter.com/UriahHarston" target="_blank"><FaTwitter  className='landingIcons' /></a>
+                            <a href="https://medium.com/@uharston" target="_blank"><FaMedium  className='landingIcons' /></a>
+                        </div>
                     </div>  
                 </div>
             </div>
